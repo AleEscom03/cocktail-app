370a1dccecf2d41b3ec35de2c08744437fd3b179
mauricio marquez <jmau.marquezd@gmail.com>
2020-09-03 14:18:24 -0500

    Se instalaron los componentes login-comp y home-lit. Además se creo el manejador de evento _login

--- src/CocktailApp.js
+++ src/CocktailApp.js
@@ -1,11 +1,15 @@
 import { LitElement, html, css } from 'lit-element';
 import { openWcLogo } from './open-wc-logo.js';
+import 'home-lit/home-lit.js';
+import 'login-comp/login-comp.js';
 
 export class CocktailApp extends LitElement {
   static get properties() {
     return {
       title: { type: String },
       page: { type: String },
+      isLogin: {type: Boolean},
+      user: { type: String},
     };
   }
 
@@ -14,9 +18,25 @@ export class CocktailApp extends LitElement {
     `;
   }
 
+  constructor(){
+    super();
+    this.isLogin = false;
+    this.user = '';
+  }
+
   render() {
     return html`
-      <h1>APP de Cocteles</h1>
+      ${!this.isLogin ?
+        html`<login-comp @login="${this._login}">login</login-comp>` :
+        html`<home-lit .user="${this.user}"></home-lit>`
+      }
     `;
+  }
+
+  _login(e){
+    this.isLogin = e.detail.access;
+    this.user = e.detail.user;
+    console.log(this.isLogin);
+    console.log(this.user);
   }
 }
